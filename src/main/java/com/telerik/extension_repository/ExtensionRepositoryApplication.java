381a1f42296eb53360be8bd5d7a72457dfeb4a30
oxanahr <oxana.hristova@gmail.com>
2018-08-16 15:20:28 +0300

    Initial project commit

---
+++ src/main/java/com/telerik/extension_repository/ExtensionRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.telerik.extension_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensionRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtensionRepositoryApplication.class, args);
+    }
+}
