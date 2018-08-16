13c0b18c93cb77581eee12e879832f84722d9dac
Rado <markov.r@abv.bg>
2018-08-16 17:23:53 +0300

    Tag entity added.

--- src/main/java/com/telerik/extension_repository/entities/Authority.java
+++ src/main/java/com/telerik/extension_repository/entities/Authority.java
@@ -1,35 +1,35 @@
-package com.telerik.extension_repository.entities;
-
-import org.springframework.security.core.GrantedAuthority;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name = "authorities")
-public class Authority implements GrantedAuthority {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    private String authority;
-
-    @Override
-    public String getAuthority() {
-        return authority;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setAuthority(String authority) {
-        this.authority = authority;
-    }
-
-
-}
+//package com.telerik.extension_repository.entities;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "authorities")
+//public class Authority implements GrantedAuthority {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private long id;
+//
+//    private String authority;
+//
+//    @Override
+//    public String getAuthority() {
+//        return authority;
+//    }
+//
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public void setAuthority(String authority) {
+//        this.authority = authority;
+//    }
+//
+//
+//}
