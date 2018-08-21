b28494cc92cf6bb21c47ebf91ea17b056fda41ef
oxanahr <oxana.hristova@gmail.com>
2018-08-21 12:43:37 +0300

    Add Extension getAll()

--- src/main/java/com/telerik/extension_repository/entities/User.java
+++ src/main/java/com/telerik/extension_repository/entities/User.java
@@ -27,7 +27,7 @@ public class User implements UserDetails {
 
     private boolean isEnabled;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_authorities",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "authority_id"))
