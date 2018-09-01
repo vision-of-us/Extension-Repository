45a74f2b8c2a82fd54f8097f501813d24cbe27d7
oxanahr <oxana.hristova@gmail.com>
2018-09-01 23:31:03 +0300

    ADD Messages class

---
+++ src/main/java/com/telerik/extension_repository/utils/Messages.java
@@ -0,0 +1,39 @@
+package com.telerik.extension_repository.utils;
+
+public final class Messages {
+
+    public static final String YOU_HAVE_NO_PERMISSION = "You have no permission to do this operation!";
+
+    public static final String NOT_FOUND = "Not Found";
+
+    public static final String EMAIL_ALREADY_EXIST = "This email already exist";
+
+    public static final String THE_PASSWORD_DOESN_T_MATCH = "The password confirmation doesn't match";
+
+    public static final String YOU_CAN_NOT_DELETE_YOUR_PROFILE = "You can't delete your profile";
+
+    public static final String SUCCESSFULLY_CREATED_ARTICLE = "Successfully created extension";
+
+    public static final String SUCCESSFULLY_CREATED_DESTINATION = "Successfully created destination";
+
+    public static final String SUCCESSFULLY_CREATED_USER = "Successfully registered user";
+
+    public static final String SUCCESSFULLY_EDITED_EXTENSION = "Successfully edited extension";
+
+    public static final String SUCCESSFULLY_DELETED_EXTENSION = "Successfully deleted extension";
+
+    public static final String SUCCESSFULLY_EDITED_USER = "Successfully edited user profile";
+
+    public static final String SUCCESSFULLY_DELETED_USER = "Successfully deleted user profile";
+
+    public static final String NEED_LOGIN = "Log in or register to perform this action!";
+
+    public static final String ALREADY_VOTED = "You already voted!";
+
+    private Messages() {
+    }
+
+    public static final String ERROR = "ERROR";
+
+    public static final String SUCCESS = "success";
+}
